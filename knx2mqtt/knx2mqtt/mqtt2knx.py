856193d0a87ab2343b6add1a2a8f646d5efb9e08
Gerrit Beine <mail@gerritbeine.com>
2022-07-30 23:21:04 +0200

    MQTT2KNX: move subscriptions to MQTT class
    The callback function on_connect handles the subscription during connection.
    This should be done in MQTT to ensure loose coupling.

--- knx2mqtt/knx2mqtt/mqtt2knx.py
+++ knx2mqtt/knx2mqtt/mqtt2knx.py
@@ -12,12 +12,11 @@ class mqtt2knx:
 		self._knx = knx
 		self._mqtt = mqtt
 		self._mqtt.set_message_cb(self.on_message)
-		self._mqtt.set_connect_cb(self.on_connect)
 
 
 	def on_message(self, client, userdata, message):
 		try:
-			logging.info("MQTT connection callback")
+			logging.debug("MQTT connection callback")
 			topic = message.topic
 			payload = str(message.payload.decode()) # ensure that payload is string
 
@@ -27,20 +26,7 @@ class mqtt2knx:
 			logging.debug("Publish value {0} for address {1}".format(payload, address))
 			self._knx.publish(address, payload)
 
-			return True
-
 		except Exception as e:
 			logging.error(traceback.format_exc())
 
-
-	def on_connect(self, client, userdata, flags, rc):
-		try:
-			logging.info("MQTT connection callback")
-
-			for topic in self._subscriptions:
-				self._mqtt.subscribe(topic)
-
-			return True
-
-		except Exception as e:
-			logging.error(traceback.format_exc())
+		return True
